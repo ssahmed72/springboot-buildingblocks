adda73b8d9789657fb2f253c19787f4723626a6e
Syed Ahmed <ssahmed72@gmail.com>
2019-11-24 21:01:27 -0600

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocks2Application.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
