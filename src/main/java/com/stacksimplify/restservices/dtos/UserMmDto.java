fe2da51b31eb7b3e6ec92e213e142eb77902cec7
Syed Ahmed <ssahmed72@gmail.com>
2019-11-26 22:06:44 -0600

    model mapper

---
+++ src/main/java/com/stacksimplify/restservices/dtos/UserMmDto.java
@@ -0,0 +1,31 @@
+package com.stacksimplify.restservices.dtos;
+
+import java.util.List;
+
+import com.stacksimplify.restservices.entities.Order;
+
+public class UserMmDto {
+	private Long id;
+	private String username;
+	private List<Order> orders; 
+	
+	public Long getId() {
+		return id;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
